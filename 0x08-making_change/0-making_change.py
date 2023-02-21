611ec996b6b3f12fd54bde15bc7fd6cb6a934980
holabayor <liasu.olabayo@gmail.com>
2023-02-21 18:50:24 +0100

    task attempt

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+Given an n x n 2D matrix, rotate it 90 degrees clockwise
+'''
+
+
+from itertools import count
+
+
+def makeChange(coins, total):
+    """ Determine the fewest number of coins needed to
+        meet a given i total
+    Args:
+        coins (list): List of integers
+        total (int): Total number of coins
+    Return:
+        Fewest number of coins needed to meet total else -1
+    """
+    totalList = [total + 1] * (total + 1)
+    # totalList[0] = 0
+    for i in range(1, total + 1):
+        # print(f'This is loop no {i}')
+        for coin in coins:
+            if i - coin >= 0:
+                totalList[i] = min(
+                    totalList[i], 1 + totalList[i - coin])
+                # print(f'The list is {totalList}')
+
+    return totalList[total] if totalList[total] != total + 1 else -1
