9aae8417980a3153072ee6651dd394902882a7d3
holabayor <liasu.olabayo@gmail.com>
2023-02-28 21:41:02 +0100

    done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,15 +2,31 @@
 '''
 Given an island, return the perimeter of the island
 '''
-
-
 from typing import List
+
 
 def island_perimeter(grid: List[List[int]]) -> int:
     ''' Calculate the perimeter of a given grid
         Returns the perimeter of the island
     '''
-    for i in grid:
-        for j in grid[i]:
-            print(grid[i][j])
-    return i * j
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:  # check if the current cell is an island (1)
+                # check if the cell to the left is water (0)
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1  # add 1 to the perimeter
+
+                # check if the cell above is water (0)
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1  # add 1 to the perimeter
+
+                # check if the cell to the right is water (0)
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1  # add 1 to the perimeter
+
+                # check if the cell below is water (0)
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1  # add 1 to the perimeter
+
+    return perimeter
